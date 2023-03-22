04599aadd34de10400aca9066eaa1f90f15a5d53
Abdullah Al Akash <aabdullahalakash@gmail.com>
2023-03-22 22:50:06 +0600

    Complete Image Problem

--- src/Pages/JoinAsLearner/JoinAsLearner.js
+++ src/Pages/JoinAsLearner/JoinAsLearner.js
@@ -12,14 +12,12 @@ const JoinAsLearner = () => {
         const handleCarType = car => {
                 setCarType(car);
         }
-        // Handle Image:
+        // Handle Profile Image:
         const [selectedFile, setSelectedFile] = useState();
-        const [isFilePicked, setIsFilePicked] = useState(false);
         const [profilePic, setProfilePic] = useState('');
 
-        const changeHandler = (event) => {
+        const handleProfilePic = (event) => {
                 setSelectedFile(event.target.files[0]);
-                setIsFilePicked(true);
         };
 
         const handleLearner = e => {
@@ -27,12 +25,14 @@ const JoinAsLearner = () => {
                 let name = e.target.name.value;
                 let email = e.target.email.value;
                 let password = e.target.password.value;
+                let confirmPassword = e.target.confirmPassword.value;
                 let age = e.target.age.value;
                 let phone = e.target.phone.value;
                 let vehicle = carType;
-                let image = selectedFile;
 
-
+                if (password !== confirmPassword) {
+                        return toast.error("Password didn't match");
+                }
 
                 const formData = new FormData();
 
@@ -44,9 +44,46 @@ const JoinAsLearner = () => {
                 })
                         .then(res => res.json())
                         .then(imgData => {
-                                console.log(imgData);
                                 setProfilePic(imgData?.data?.url);
+                                // console.log(imgData?.data?.url)
+                                const member = {
+                                        name: name,
+                                        email: email,
+                                        password: password,
+                                        age: age,
+                                        phone: phone,
+                                        vehicle: vehicle,
+                                        image: imgData?.data?.url
+                                }
+                                console.log(member);
+                                const url = 'http://localhost:5000/registration';
+                                fetch(url, {
+                                        method: "POST",
+                                        headers: {
+                                                "content-type": "application/json"
+                                        },
+                                        body: JSON.stringify(member)
+                                })
+                                        .then(res => res.json())
+                                        .then(result => {
+                                                console.log(result)
+                                                if (result.acknowledged) {
+
+                                                        toast.success("Registration Successfully Done!");
+
+
+                                                }
+                                                else {
+                                                        toast("Something Went Wrong. Please ry again!")
+                                                }
+                                        })
+
                         })
+
+
+
+
+
 
 
                 // Error Handling:
@@ -58,43 +95,14 @@ const JoinAsLearner = () => {
                         setEmailError("Enter Valid Email!")
                         return;
                 }
-                const member = {
-                        name: name,
-                        email: email,
-                        password: password,
-                        age: age,
-                        phone: phone,
-                        vehicle: vehicle,
-                        image: profilePic
-                }
-                console.log(member);
-                // const url = 'http://localhost:5000/registration';
-                // fetch(url, {
-                //         method: "POST",
-                //         headers: {
-                //                 "content-type": "application/json"
-                //         },
-                //         body: JSON.stringify(member)
-                // })
-                //         .then(res => res.json())
-                //         .then(result => {
-                //                 console.log(result)
-                //                 if (result.acknowledged) {
-
-                //                         toast.success("Registration Successfully Done!");
 
 
-                //                 }
-                //                 else {
-                //                         toast("Something Went Wrong. Please ry again!")
-                //                 }
-                //         })
 
                 setPasswordError('');
                 setEmailError('')
         }
         return (
-                <div className="flex justify-center items-center h-screen">
+                <div className="flex justify-center items-center mt-8 pb-8">
                         <div className="card w-96 bg-base-100 shadow-xl">
                                 <div className="card-body">
                                         <h2 className="text-center font-bold text-2xl">Please Complete The Form</h2>
@@ -102,17 +110,21 @@ const JoinAsLearner = () => {
                                                 <input type="text" name="name" placeholder="Your Name" className="input input-bordered w-full max-w-xs mt-8" required />
                                                 <input type="email" name="email" placeholder="Your Email" className="input input-bordered w-full max-w-xs mt-4" required />
                                                 <h3 className="text-red-600 pl-2">{emailError ? emailError : ''}</h3>
-                                                <input type="password" name="password" placeholder="Your Password" className="input input-bordered w-full max-w-xs mt-4" required />
+
                                                 <input type="number" name="age" placeholder="Your Age" className="input input-bordered w-full max-w-xs mt-8" required />
                                                 <input type="text" name="address" placeholder="Your Address" className="input input-bordered w-full max-w-xs mt-8" required />
                                                 <input type="text" name="phone" placeholder="Your Phone Number" className="input input-bordered w-full max-w-xs mt-8" required />
                                                 <h3 className='pt-3 fw-bold'>Your Profile Picture:</h3>
-                                                <input type="file" onChange={changeHandler} placeholder="Your Profile Picture" className=" max-w-xs mt-2" required />
+                                                <input type="file" onChange={handleProfilePic} placeholder="Your Profile Picture" className=" max-w-xs mt-2" required />
+                                                {/* <h3 className='pt-3 fw-bold'>Your NID Picture:</h3>
+                                                <input type="file" onChange={handleNIDPic} placeholder="Your Profile Picture" className=" max-w-xs mt-2" required /> */}
                                                 <h3 className='pb-3 pt-3 fw-bold'>Select Vehicle Type:</h3>
                                                 <select className="form-select" aria-label="Default select example" onChange={e => handleCarType(e.target.value)} value={carType}>
                                                         <option value="car">Car</option>
                                                         <option value="bike">Bike</option>
                                                 </select>
+                                                <input type="password" name="password" placeholder="Password" className="input input-bordered w-full max-w-xs mt-4" required />
+                                                <input type="password" name="confirmPassword" placeholder="Confirm Password" className="input input-bordered w-full max-w-xs mt-4" required />
                                                 <h3 className="text-red-600 pl-2">{passwordError ? passwordError : ''}</h3>
                                                 <input type="submit" value="Registration" className="btn input-bordered w-full max-w-xs mt-4" />
 
